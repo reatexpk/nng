8c0499412d6713f4053cba0dd83b758dd32ab452
Лебедев Дмитрий Сергеевич <dslebedev@alfabank.ru>
2020-09-22 22:48:00 +0500

    refactor(*): change validation messages & validation function

--- src/helpers.ts
+++ src/helpers.ts
@@ -16,9 +16,5 @@ export function getFullName(from: From): string {
 
 export function validateMessage(message: Context["message"]): boolean {
   if (!message || !message.text) return false;
-
-  if (message.text.length > 60) return false;
-
-  const regexp = /[^а-яё\d.,!?:_\-–=+*&@#%\s]/gi;
-  return !regexp.test(message.text);
+  return message.text.length <= 60;
 }

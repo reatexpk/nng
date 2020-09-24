0007c0971c3bc621bc783e7ebbfc4e260cc116ae
Лебедев Дмитрий Сергеевич <dslebedev@alfabank.ru>
2020-09-24 22:30:13 +0500

    fix(*): change path to static

--- src/frontend/server.ts
+++ src/frontend/server.ts
@@ -4,10 +4,10 @@ import path from "path";
 const app = express();
 const port = 3000;
 
-app.use("/static", express.static(path.join(__dirname, "/static")));
+app.use("/", express.static(path.join(__dirname, "/../../dist/")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(`${__dirname}/index.html`));
+  res.sendFile(path.join(`${__dirname}/../../dist/index.html`));
 });
 
 app.listen(port, () => {

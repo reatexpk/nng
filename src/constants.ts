739dae16e96867559bae495bd032888aef07b3bb
Лебедев Дмитрий Сергеевич <dslebedev@alfabank.ru>
2020-09-25 02:15:04 +0500

    fix(*): change constants and ports

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ export const DELAY = 60;
 export const LIMIT = 60;
 export const MARQUEE_SPEED = 100;
 export const SPACES_COUNT = 50;
-export const WS_PORT = 9999;
+export const WS_PORT = 3001;
 export const SUCCESS = "Сообщение отправлено";
 export const INVALID_MESSAGE =
   "Невалидное сообщение. Сообщение не должно быть длиннее 60 символов, содержать медиа-файлы и стикеры";

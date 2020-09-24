5b8ef4e397e817f6b8aeda5a0f76e769a87040e9
Dmitry Lebedev <32232046+reatexpk@users.noreply.github.com>
2020-09-24 22:39:30 +0500

    Feature/deploy-preparing (#10)
    * feat(*): set real youtube playlist link
    
    * fix(*): change path to static
    
    * fix(*): fix scripts
    
    Co-authored-by: Лебедев Дмитрий Сергеевич <dslebedev@alfabank.ru>

--- src/frontend/server.ts
+++ src/frontend/server.ts
@@ -4,10 +4,10 @@ import path from "path";
 const app = express();
 const port = 3000;
 
-app.use("/", express.static(path.join(__dirname, `../../dist`)));
+app.use("/", express.static(path.join(__dirname, "/../../dist/")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, `../../dist/index.html`));
+  res.sendFile(path.join(`${__dirname}/../../dist/index.html`));
 });
 
 app.listen(port, () => {
